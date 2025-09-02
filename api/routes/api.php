9b9072ed2894372849f22553b2dcc05b51b7c404
Ivan Kirby Colina <ivan.kirby.colina@gmail.com>
2025-09-02 23:24:11 +0800

    Add my-listing route

--- api/routes/api.php
+++ api/routes/api.php
@@ -11,11 +11,13 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+    Route::get('/user', function () {
+        return auth()->user();
     })->name('user');
 
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('subcategories', SubcategoryController::class);
     Route::apiResource('listings', ListingController::class);
+
+    Route::get('/my-listings', [ListingController::class, 'myListings'])->name('my-listings');
 });
